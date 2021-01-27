7744abdcaf761fe082436ad367f126d45d75e929
Andy De George (adegeo) <adegeo@microsoft.com>
2021-01-27 13:19:14 -0800

    Finish article for new project.

---
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/LayoutStep2.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for LayoutStep2.xaml
+    /// </summary>
+    public partial class LayoutStep2 : Window
+    {
+        public LayoutStep2()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/vb/MoreControls3.xaml.vb
@@ -0,0 +1,5 @@
+﻿Public Class MoreControls3
+    Private Sub ButtonAddName_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+End Class
