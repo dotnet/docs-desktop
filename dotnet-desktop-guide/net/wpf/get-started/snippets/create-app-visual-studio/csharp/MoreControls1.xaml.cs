b196e03b476adecc3dba12f53b282338e47ce4db
Andy De George <67293991+adegeo@users.noreply.github.com>
2021-01-28 08:41:01 -0800

    New article: Create new WPF project (#183)
    * Basic index file
    
    * Finish article for new project.
    
    * acro
    
    * markdown fix
    
    * Fix build errors
    
    * fix code lang
    
    * fix code lang

---
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/MoreControls1.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for MoreControls1.xaml
+    /// </summary>
+    public partial class MoreControls1 : Window
+    {
+        public MoreControls1()
+        {
+            InitializeComponent();
+        }
+    }
+}
