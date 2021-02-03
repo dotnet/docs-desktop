fb0c8caad283ffa3fe18de548c24553c91f60e82
Andy De George <67293991+adegeo@users.noreply.github.com>
2021-02-02 20:25:07 -0800

    Add new create wpf app article (#193)

---
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/LayoutStep3.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for LayoutStep3.xaml
+    /// </summary>
+    public partial class LayoutStep3 : Window
+    {
+        public LayoutStep3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/MoreControls3.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Names
+{
+    /// <summary>
+    /// Interaction logic for MoreControls3.xaml
+    /// </summary>
+    public partial class MoreControls3 : Window
+    {
+        public MoreControls3()
+        {
+            InitializeComponent();
+        }
+
+        //<ButtonEvent>
+        private void ButtonAddName_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        //</ButtonEvent>
+    }
+}
