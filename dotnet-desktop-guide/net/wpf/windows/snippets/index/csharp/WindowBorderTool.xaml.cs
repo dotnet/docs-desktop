64e66d15239b6307569aeab7521a0da38d71a7ba
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2021-03-12 09:01:34 -0800

    Port Window Overview WPF article (#1011)
    * Add Window overview article
    
    * Add TOC
    
    * Remove temp code
    
    * Fix headers

---
+++ dotnet-desktop-guide/net/wpf/windows/snippets/index/csharp/WindowBorderTool.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WindowsOverview
+{
+    /// <summary>
+    /// Interaction logic for WindowBorderTool.xaml
+    /// </summary>
+    public partial class WindowBorderTool : Window
+    {
+        public WindowBorderTool()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dotnet-desktop-guide/net/wpf/windows/snippets/index/vb/ClosingWindow.xaml.vb
@@ -0,0 +1,6 @@
+﻿Public Class ClosingWindow
+    Private Sub fileExitMenuItem_Click(sender As Object, e As RoutedEventArgs)
+        ' Close the current window
+        Me.Close()
+    End Sub
+End Class
