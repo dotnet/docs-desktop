28406b0be5cf1d277710cd8cb8bc6719292ced5b
Andy De George (adegeo) <adegeo@microsoft.com>
2021-03-12 13:18:39 -0800

    Initial test commit

---
+++ dotnet-desktop-guide/net/wpf/windows/snippets/dialog-boxes-overview/csharp/Window1.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WindowsOverview
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // <YesNoCancel>
+            string messageBoxText = "Do you want to save changes?";
+            string caption = "Word Processor";
+            MessageBoxButton button = MessageBoxButton.YesNoCancel;
+            MessageBoxImage icon = MessageBoxImage.Warning;
+            MessageBoxResult result;
+
+            // <MessageBoxShow>
+            result = MessageBox.Show(messageBoxText, caption, button, icon);
+            // </YesNoCancel>
+
+            switch (result)
+            {
+                case MessageBoxResult.Cancel:
+                    break;
+                case MessageBoxResult.Yes:
+                    break;
+                case MessageBoxResult.No:
+                    break;
+                default:
+                    break;
+            }
+            // </MessageBoxShow>
+        }
+    }
+}
---
+++ dotnet-desktop-guide/net/wpf/windows/snippets/dialog-boxes-overview/vb/Window1.xaml.vb
@@ -0,0 +1,16 @@
+﻿Public Class Window1
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        '<YesNoCancel>
+        Dim messageBoxText As String = "Do you want to save changes?"
+        Dim caption As String = "Word Processor"
+        Dim Button As MessageBoxButton = MessageBoxButton.YesNoCancel
+        Dim Icon As MessageBoxImage = MessageBoxImage.Warning
+        Dim result As MessageBoxResult
+
+        '<MessageBoxShow>
+        result = MessageBox.Show(messageBoxText, caption, Button, Icon)
+        '</YesNoCancel>
+
+        '</MessageBoxShow>
+    End Sub
+End Class
