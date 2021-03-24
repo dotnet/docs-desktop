1a6cde2004d58bceb85e76963324676c6573b9c7
Andy De George (adegeo) <adegeo@microsoft.com>
2021-03-24 14:25:05 -0700

    Add get/set main window

---
+++ dotnet-desktop-guide/net/wpf/windows/snippets/how-to-get-set-main-application-window/csharp/Window1.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace MainApp
+{
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        // <GetMainWindow>
+        private void Button_Click(object sender, RoutedEventArgs e) =>
+            MessageBox.Show($"The main window's title is: {Application.Current.MainWindow.Title}");
+        // </GetMainWindow>
+    }
+}
