ef7af3251783220dd62c78e511659bbe8b4f6407
Andy De George (adegeo) <adegeo@microsoft.com>
2021-04-02 14:44:35 -0700

    initial resources docs

---
+++ dotnet-desktop-guide/net/wpf/systems/snippets/xaml-resources-and-code/vb/MainWindow.xaml.vb
@@ -0,0 +1,8 @@
+﻿Class MainWindow
+    '<ButtonBrush>
+    Private Sub myButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim buttonControl = DirectCast(sender, Button)
+        buttonControl.Background = DirectCast(Me.FindResource("RainbowBrush"), Brush)
+    End Sub
+    '</ButtonBrush>
+End Class
