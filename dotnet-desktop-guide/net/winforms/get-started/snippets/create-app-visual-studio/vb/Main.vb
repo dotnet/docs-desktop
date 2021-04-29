51a3b107d4db76171e7f32c658d67917750039e9
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2021-04-29 08:59:00 -0700

    Update Main.vb

--- dotnet-desktop-guide/net/winforms/get-started/snippets/create-app-visual-studio/vb/Main.vb
+++ dotnet-desktop-guide/net/winforms/get-started/snippets/create-app-visual-studio/vb/Main.vb
@@ -14,6 +14,7 @@ Partial Public Class Form1
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         If Not String.IsNullOrWhiteSpace(txtName.Text) And Not lstNames.Items.Contains(txtName.Text) Then
             lstNames.Items.Add(txtName.Text)
+            lstNames.Items.Add(txtName.Text)
         End If
     End Sub
 End Class
