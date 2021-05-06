3885aab154ae945956df9e15baa44f9b6ccc06b5
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2021-05-06 08:03:42 -0700

    Add how to bind enum article (#1056)
    * add how to article
    
    * Fix lint
    
    * Fix the code example
    
    * Add missing code
    
    * Update how-to-bind-to-an-enumeration.md
    
    * Update dotnet-desktop-guide/net/wpf/data/how-to-bind-to-an-enumeration.md
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>
    
    Co-authored-by: Tom Dykstra <tdykstra@microsoft.com>

---
+++ dotnet-desktop-guide/net/wpf/data/snippets/how-to-bind-to-an-enumeration/vb/BindEnum.xaml.vb
@@ -0,0 +1,7 @@
+﻿Public Class BindEnum
+    Public Sub Method1()
+        '<EnumGetValues>
+        Dim enumDataSource = System.Enum.GetValues(GetType(System.Windows.HorizontalAlignment))
+        '</EnumGetValues>
+    End Sub
+End Class
