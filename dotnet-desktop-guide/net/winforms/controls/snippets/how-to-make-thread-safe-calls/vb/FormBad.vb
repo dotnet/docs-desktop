3bf10712ef45458e23ebecee03f01c8c8b3d074d
Andy De George (adegeo) <adegeo@microsoft.com>
2021-07-21 10:25:32 -0700

    add how-to-make-thread-safe-calls

---
+++ dotnet-desktop-guide/net/winforms/controls/snippets/how-to-make-thread-safe-calls/vb/FormBad.vb
@@ -0,0 +1,12 @@
+﻿Public Class FormBad
+    '<Bad>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim thread2 As New System.Threading.Thread(AddressOf WriteTextUnsafe)
+        thread2.Start()
+    End Sub
+
+    Private Sub WriteTextUnsafe()
+        TextBox1.Text = "This text was set unsafely."
+    End Sub
+    '</Bad>
+End Class
---
+++ dotnet-desktop-guide/net/winforms/controls/snippets/how-to-make-thread-safe-calls/vb/FormThread.vb
@@ -0,0 +1,28 @@
+﻿Public Class FormThread
+    '<Good>
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim threadParameters As New System.Threading.ThreadStart(Sub()
+                                                                     WriteTextSafe("This text was set safely.")
+                                                                 End Sub)
+
+        Dim thread2 As New System.Threading.Thread(threadParameters)
+        thread2.Start()
+
+    End Sub
+
+    Private Sub WriteTextSafe(text As String)
+
+        If (TextBox1.InvokeRequired) Then
+
+            TextBox1.Invoke(Sub()
+                                WriteTextSafe($"{text} (THREAD2)")
+                            End Sub)
+
+        Else
+            TextBox1.Text = text
+        End If
+
+    End Sub
+    '</Good>
+End Class
