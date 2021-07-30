6128fee9ca170f9ca86e536260006452b7bd8d8c
Zoltán Szatmáry <40998048+Zotyamester@users.noreply.github.com>
2021-07-30 19:11:24 +0200

    Modify snippet in a WPF getting started (#1127)

--- dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/Final.xaml.cs
+++ dotnet-desktop-guide/net/wpf/get-started/snippets/create-app-visual-studio/csharp/Final.xaml.cs
@@ -28,7 +28,10 @@ namespace Names
         private void ButtonAddName_Click(object sender, RoutedEventArgs e)
         {
             if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+            {
                 lstNames.Items.Add(txtName.Text);
+                txtName.Clear();
+            }
         }
         //</FinalCode>
     }
