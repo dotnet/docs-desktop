e56d12100a85a8a2f804c963cec66b6b0e2cef97
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2021-08-20 17:44:24 -0500

    Content update including C# WPF project

--- dotnet-desktop-guide/net/wpf/documents/how-to-display-print-dialog.md
+++ dotnet-desktop-guide/net/wpf/documents/how-to-display-print-dialog.md
@@ -1,28 +1,40 @@
 ---
 title: How to display a print dialog
-description: 
-ms.date: 
+description: Learn how to print from your application using the System.Windows.Controls.PrintDialog class to open a standard Microsoft Windows print dialog box.
+ms.date: 08/20/2021
 dev_langs: 
   - "csharp"
   - "vb"
 helpviewer_keywords: 
-  - 
+  - "invoking print dialogs [WPF]"
+  - "print dialogs [WPF], invoking"
 ---
 
 # How to display a print dialog box (WPF .NET)
 
-<!--
-
-Source material for this article:
-- https://docs.microsoft.com/en-us/dotnet/desktop/wpf/advanced/how-to-invoke-a-print-dialog?view=netframeworkdesktop-4.8
-
-Notes:
-- None
-
--->
-
-!!INTRO
+Want to print from your application? You can use the <xref:System.Windows.Controls.PrintDialog> class to open a standard Microsoft Windows print dialog box. Here's how.
 
 [!INCLUDE [desktop guide under construction](../../includes/desktop-guide-preview-note.md)]
 
-!!STUFF
+## Example
+
+The <xref:System.Windows.Controls.PrintDialog> class provides a single control for print configuration and XPS job submission. The control is easy to use and can be instantiated using XAML markup or code. This example creates and displays a PrintDialog instance using code.
+
+[!code-csharp[SnippetTextBlocksExplicit](./snippets/how-to-display-print-dialog/csharp/MainWindow.xaml.cs)]
+  
+> [!NOTE]
+> The <xref:System.Windows.Controls.PrintDialog?displayProperty=nameWithType> control used for WPF and discussed here, should not be confused with the <xref:System.Windows.Forms.PrintDialog?displayProperty=nameWithType> component of Windows Forms.  
+  
+Users can use the print dialog to:
+
+- Select from the printers installed on their computer. The **Microsoft XPS Document Writer** option creates an XML Paper Specification (XPS) file.
+- Print only a specific range of pages.
+
+You can even use the <xref:System.Windows.Controls.PrintDialog.PrintDocument%2A> method to print without opening the print dialog. But for performance reasons, it's better to use the <xref:System.Printing.PrintQueue.AddJob%2A> method, or one of the many <xref:System.Windows.Xps.XpsDocumentWriter.Write%2A> and <xref:System.Windows.Xps.XpsDocumentWriter.WriteAsync%2A> methods of the <xref:System.Windows.Xps.XpsDocumentWriter>. For more about this, see [Programmatically Print XPS Files](\how-to-print-xps-files.md).
+  
+## See also
+
+- <xref:System.Windows.Controls.PrintDialog>
+- [Documents in WPF](documents-in-wpf.md)
+- [Printing Overview](printing-overview.md)
+- [Microsoft XPS Document Writer](/windows/win32/printdocs/microsoft-xps-document-writer)
---
+++ dotnet-desktop-guide/net/wpf/documents/snippets/how-to-display-print-dialog/csharp/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Xps.Packaging;
+using Microsoft.Win32;
+
+namespace CodeSampleCsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Events
+            btnBrowse.Click += BtnBrowse_Click;
+            btnPrint.Click += BtnPrint_Click;
+        }
+
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // Configure an open file dialog box
+            OpenFileDialog openFileDialog = new()
+            {
+                FileName = "Document", // Default file name
+                DefaultExt = ".xps", // Default file extension
+                Filter = "Text documents (.xps)|*.xps" // Filter files by extension
+            };
+
+            // Show open file dialog box.
+            bool? result = openFileDialog.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                // Show selected XPS file path
+                txtXpsFilePath.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void BtnPrint_Click(object sender, RoutedEventArgs e)
+        {
+            // Check the file exists
+            if (!File.Exists(txtXpsFilePath.Text))
+            {
+                _ = MessageBox.Show("First select an XPS file.");
+                return;
+            }
+
+            // Create the print dialog object and set options
+            PrintDialog printDialog = new()
+            {
+                PageRangeSelection = PageRangeSelection.AllPages,
+                UserPageRangeEnabled = true
+            };
+
+            // Display the dialog. This returns true if the user presses the Print button
+            bool? isPrinted = printDialog.ShowDialog();
+            if (isPrinted != true)
+            {
+                return;
+            }
+
+            // Print the document to an XPS or OXPS file
+            try
+            {
+                // Open the selected file
+                XpsDocument xpsDocument = new(txtXpsFilePath.Text, FileAccess.Read);
+
+                // Support creation of multiple-page elements
+                FixedDocumentSequence fixedDocSeq = xpsDocument.GetFixedDocumentSequence();
+
+                // Launch a 'Save Print Output As' dialog, and then print to the specified output file
+                printDialog.PrintDocument(null, $"Printing {Path.GetFileName(txtXpsFilePath.Text)}");
+            }
+            catch (Exception e2)
+            {
+                _ = MessageBox.Show(e2.Message);
+            }
+        }
+    }
+}
