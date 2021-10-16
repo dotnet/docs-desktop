f991fd0eb879c29ce959f808b35a3254a206d9b5
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2021-10-15 19:13:16 -0500

    Add article, code projects, toc, and redirects

---
+++ dotnet-desktop-guide/net/wpf/properties/snippets/how-to-implement-a-dependency-property/csharp/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+
+namespace CodeSampleCsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+    }
+
+    // <DefineDependencyProperty>
+    public class Aquarium : DependencyObject
+    {
+        public static readonly DependencyProperty HasFishProperty =
+            DependencyProperty.Register(
+                name: "HasFish",
+                propertyType: typeof(bool),
+                ownerType: typeof(Aquarium),
+                typeMetadata: new FrameworkPropertyMetadata(defaultValue: false));
+
+        public bool HasFish
+        {
+            get => (bool)GetValue(HasFishProperty);
+            set => SetValue(HasFishProperty, value);
+        }
+    }
+    // </DefineDependencyProperty>
+}
---
+++ dotnet-desktop-guide/net/wpf/properties/snippets/how-to-implement-a-dependency-property/vb/MainWindow.xaml.vb
@@ -0,0 +1,32 @@
+﻿Namespace CodeSampleVb
+    Partial Public Class MainWindow
+        Inherits Window
+
+        Public Sub New()
+            InitializeComponent()
+        End Sub
+
+        ' <DefineDependencyProperty>
+        Public Class Aquarium
+            Inherits DependencyObject
+
+            Public Shared ReadOnly HasFishProperty As DependencyProperty =
+            DependencyProperty.Register(
+                name:="HasFish",
+                propertyType:=GetType(Boolean),
+                ownerType:=GetType(Aquarium),
+                typeMetadata:=New FrameworkPropertyMetadata(defaultValue:=False))
+
+            Public Property HasFish As Boolean
+                Get
+                    Return GetValue(HasFishProperty)
+                End Get
+                Set(value As Boolean)
+                    SetValue(HasFishProperty, value)
+                End Set
+            End Property
+
+        End Class
+        ' </DefineDependencyProperty>
+    End Class
+End Namespace
