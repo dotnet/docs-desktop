71ffc47742c001b1dfcc0ccd007f91f96886f81b
Andy De George (adegeo) <adegeo@microsoft.com>
2021-11-15 11:13:15 -0800

    Migrate code to .NET 6

---
+++ dotnet-desktop-guide/net/winforms/get-started/snippets/create-app-visual-studio/csharp/Form1.cs
@@ -0,0 +1,16 @@
+namespace Names
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtName.Text) && !lstNames.Items.Contains(txtName.Text))
+                lstNames.Items.Add(txtName.Text);
+        }
+    }
+}
--- dotnet-desktop-guide/net/winforms/get-started/snippets/create-app-visual-studio/vb/Form1.vb
+++ dotnet-desktop-guide/net/winforms/get-started/snippets/create-app-visual-studio/vb/Form1.vb
@@ -1,16 +1,4 @@
-﻿Imports System.Windows.Forms
-Imports System.Drawing
-
-Partial Public Class Form1
-
-    Public Sub New()
-        InitializeComponent()
-    End Sub
-
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-    End Sub
-
+﻿Public Class Form1
     Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
         If Not String.IsNullOrWhiteSpace(txtName.Text) And Not lstNames.Items.Contains(txtName.Text) Then
             lstNames.Items.Add(txtName.Text)
