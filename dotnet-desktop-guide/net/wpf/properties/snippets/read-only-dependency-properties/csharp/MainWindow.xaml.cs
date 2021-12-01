bf2bd7147f35f0ef3b6f71c5b6843a148731f58a
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2021-11-30 18:09:04 -0600

    Add article, snippets, toc, and redirects

---
+++ dotnet-desktop-guide/net/wpf/properties/snippets/read-only-dependency-properties/csharp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+
+namespace CodeSampleCsharp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        readonly Aquarium _aquarium = new();
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Test setting a value.
+            _aquarium.SetValue(Aquarium.FishCountPropertyKey, _aquarium.FishCount + 1);
+            lblFishCount.Content = $"Aquarium fish count: {_aquarium.FishCount}";
+        }
+    }
+
+    //<RegisterReadOnlyDependencyProperty>
+    public class Aquarium : DependencyObject
+    {
+        // Register a dependency property with the specified property name,
+        // property type, owner type, and property metadata.
+        // Assign DependencyPropertyKey to a nonpublic field.
+        internal static readonly DependencyPropertyKey FishCountPropertyKey =
+            DependencyProperty.RegisterReadOnly(
+              name: "FishCount",
+              propertyType: typeof(int),
+              ownerType: typeof(Aquarium),
+              typeMetadata: new FrameworkPropertyMetadata());
+
+        // Declare a public get accessor.
+        public int FishCount =>
+            (int)GetValue(FishCountPropertyKey.DependencyProperty);
+    }
+    //</RegisterReadOnlyDependencyProperty>
+}
