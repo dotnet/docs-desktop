0358914363282fb51785c5cd5e7c3ffba6fa31af
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2022-01-24 11:07:43 -0800

    Upgrade framework winforms-outlook article (#1280)
    * upgrade winforms-outlook article
    
    * change readme name; add description
    
    * change readme name; add description

---
+++ dotnet-desktop-guide/framework/winforms/controls/snippets/how-to-create-a-multipane-user-interface-with-windows-forms/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class Form1 : Form
+    {
+        //Windows Form application
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
