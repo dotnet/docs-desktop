4d6be5ccf08132b6ea875156bffdadbbf1bda8a5
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2022-04-20 17:53:34 -0500

    Add article, code projects, toc, and redirects

---
+++ dotnet-desktop-guide/net/wpf/events/snippets/attached-events-overview/WpfControlLibraryCsharp/AquariumFilter.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace WpfControlCsharp
+{
+    //<AddRemoveHandlersForAttachedEvent>
+    public class AquariumFilter
+    {
+        // Register a custom routed event using the bubble routing strategy.
+        public static readonly RoutedEvent CleanEvent = EventManager.RegisterRoutedEvent(
+            "Clean", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(AquariumFilter));
+
+        // Provide an add handler accessor method for the Clean event.
+        public static void AddCleanHandler(DependencyObject dependencyObject, RoutedEventHandler handler)
+        {
+            if (dependencyObject is not UIElement uiElement)
+                return;
+
+            uiElement.AddHandler(CleanEvent, handler);
+        }
+
+        // Provide a remove handler accessor method for the Clean event.
+        public static void RemoveCleanHandler(DependencyObject dependencyObject, RoutedEventHandler handler)
+        {
+            if (dependencyObject is not UIElement uiElement)
+                return;
+
+            uiElement.RemoveHandler(CleanEvent, handler);
+        }
+    }
+    //</AddRemoveHandlersForAttachedEvent>
+}
