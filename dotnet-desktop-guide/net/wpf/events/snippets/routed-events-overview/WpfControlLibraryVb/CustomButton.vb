efdcdaf7b4b79f05766d258d0e0b5290b5b8a70e
v-trisshores <86677757+v-trisshores@users.noreply.github.com>
2022-04-29 14:33:44 -0500

    Add article, code, toc, and redirects.

---
+++ dotnet-desktop-guide/net/wpf/events/snippets/routed-events-overview/WpfControlLibraryVb/CustomButton.vb
@@ -0,0 +1,53 @@
+﻿Imports System.Windows
+Imports System.Windows.Controls
+
+'<CustomButton>
+Public Class CustomButton
+    Inherits Button
+
+    ' Register a custom routed event with the Bubble routing strategy.
+    Public Shared ReadOnly ConditionalClickEvent As RoutedEvent = EventManager.RegisterRoutedEvent(
+        name:="ConditionalClick",
+        routingStrategy:=RoutingStrategy.Bubble,
+        handlerType:=GetType(RoutedEventHandler),
+        ownerType:=GetType(CustomButton))
+
+    ' Provide CLR accessors to support event handler assignment.
+    Public Custom Event ConditionalClick As RoutedEventHandler
+
+        AddHandler(value As RoutedEventHandler)
+            [AddHandler](ConditionalClickEvent, value)
+        End AddHandler
+
+        RemoveHandler(value As RoutedEventHandler)
+            [RemoveHandler](ConditionalClickEvent, value)
+        End RemoveHandler
+
+        RaiseEvent(sender As Object, e As RoutedEventArgs)
+            [RaiseEvent](e)
+        End RaiseEvent
+
+    End Event
+
+    Private Sub RaiseCustomRoutedEvent()
+
+        ' Create a RoutedEventArgs instance.
+        Dim routedEventArgs As New RoutedEventArgs(routedEvent:=ConditionalClickEvent)
+
+        ' Raise the event, which will bubble up through the element tree.
+        [RaiseEvent](routedEventArgs)
+
+    End Sub
+
+    ' For demo purposes, we use the Click event as a trigger.
+    Protected Overrides Sub OnClick()
+
+        ' Some condition combined with the Click event will trigger the ConditionalClick event.
+        If Date.Now > New DateTime() Then RaiseCustomRoutedEvent()
+
+        ' Call the base class OnClick() method so Click event subscribers are notified.
+        MyBase.OnClick()
+
+    End Sub
+End Class
+'</CustomButton>
---
+++ dotnet-desktop-guide/net/wpf/events/snippets/routed-events-overview/csharp/MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+
+namespace CodeSampleCsharp
+{
+    //<EventSuppressionWorkarounds>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //<AddHandler_ToAttachHandler>
+            // Attach a Click event handler to a button using the AddHandler method.
+            Button1.AddHandler(ButtonBase.ClickEvent, new RoutedEventHandler(Button_Click));
+            //</AddHandler_ToAttachHandler>
+
+            //<LanguageSpecificSyntax_ToAttachHandler>
+            // Attach a Click event handler to a button using language specific syntax.
+            Button1.Click += Button_Click;
+            //</LanguageSpecificSyntax_ToAttachHandler>
+        }
+
+        //<ButtonsParentHandler>
+        private void YesNoCancel_Click(object sender, RoutedEventArgs e)
+        {
+            FrameworkElement feSource = e.Source as FrameworkElement;
+            switch (feSource.Name)
+            {
+                case "YesButton":
+                    // YesButton logic.
+                    break;
+                case "NoButton":
+                    // NoButton logic.
+                    break;
+                case "CancelButton":
+                    // CancelButton logic.
+                    break;
+            }
+            e.Handled = true;
+        }
+        //</ButtonsParentHandler>
+
+        //<ButtonClickHandler>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Click event logic.
+        }
+        //</ButtonClickHandler>
+
+        private void ApplyButtonStyle(object sender, RoutedEventArgs e)
+        {
+            // Logic to apply a button style.
+            ((Button)sender).Background = Brushes.Green;
+        }
+
+        private void StackPanel1_ButtonClick(object sender, RoutedEventArgs e)
+        {
+            // Button.Click event logic.
+        }
+    }
+}
