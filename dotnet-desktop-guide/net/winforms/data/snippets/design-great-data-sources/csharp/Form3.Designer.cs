d06cdfae1e051c40331811fa2b27be02c3ac89b9
Rachana Satao <83084319+rachana-satao@users.noreply.github.com>
2022-06-03 02:06:33 +0530

    New Article -1888235-2 (#1378)
    * New Article -1888235-2
    
    * Link update
    
    * link update
    
    * code update
    
    * code update
    
    * code update
    
    * Added code snippets
    
    * Code snippets updations
    
    * update after review
    
    * update in toc
    
    * toc update
    
    * update after review
    
    * link update
    
    * update in contents
    
    * Update design-great-data-sources.md
    
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>

---
+++ dotnet-desktop-guide/net/winforms/data/snippets/design-great-data-sources/csharp/Form3.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace binding_control_example
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.Load += new System.EventHandler(this.Form3_Load);
+            this.changeItemBtn.Click +=
+                new EventHandler(changeItemBtn_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ dotnet-desktop-guide/net/winforms/data/snippets/design-great-data-sources/vb/Form1.vb
@@ -0,0 +1,47 @@
+﻿'<bind_controls_using_BindingSource>
+Public Class Form1
+    Private Sub InitializeControlsAndDataSource()
+
+        ' Add a table and column to DataSet.
+        set1.Tables.Add("Menu")
+        set1.Tables(0).Columns.Add("Beverages")
+
+        ' Add some rows to the table.
+        set1.Tables(0).Rows.Add("coffee")
+        set1.Tables(0).Rows.Add("tea")
+        set1.Tables(0).Rows.Add("hot chocolate")
+        set1.Tables(0).Rows.Add("milk")
+        set1.Tables(0).Rows.Add("orange juice")
+
+        ' Set the data source to the DataSet.
+        BindingSource1.DataSource = set1
+
+        'Set the DataMember to the Menu table.
+        BindingSource1.DataMember = "Menu"
+
+        ' Add the control data bindings.
+        DataGridView1.DataSource = BindingSource1
+        TextBox1.DataBindings.Add("Text", BindingSource1, "Beverages",
+            True, DataSourceUpdateMode.OnPropertyChanged)
+        TextBox2.DataBindings.Add("Text", BindingSource1, "Beverages",
+            True, DataSourceUpdateMode.OnPropertyChanged)
+
+    End Sub
+
+    Private Sub BindingSource1_BindingComplete(ByVal sender As Object,
+        ByVal e As BindingCompleteEventArgs) Handles BindingSource1.BindingComplete
+
+        ' Check if the data source has been updated, and that no error has occurred.
+        If e.BindingCompleteContext = BindingCompleteContext.DataSourceUpdate _
+            AndAlso e.Exception Is Nothing Then
+
+            ' If not, end the current edit.
+            e.Binding.BindingManagerBase.EndCurrentEdit()
+        End If
+    End Sub
+    '</bind_controls_using_BindingSource>
+    
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        InitializeControlsAndDataSource()
+    End Sub
+End Class
