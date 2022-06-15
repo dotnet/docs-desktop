10ffdc48ddcb09af5e0a4677f52a692272796fff
Rachana Satao <83084319+rachana-satao@users.noreply.github.com>
2022-06-15 21:19:43 +0530

    New article-Synchronize-multiple-controls (#1393)
    * New article-Syn-multiple-controls
    
    * update after review
    
    * update after review
    
    * ToC update
    
    * Update how-to-synchronize-multiple-controls.md
    
    * Update definitions.json
    
    * Update .openpublishing.redirection.json
    
    Co-authored-by: Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>

---
+++ dotnet-desktop-guide/net/winforms/data/snippets/how-to-synchronize-multiple-controls/csharp/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿using System.Data;
+
+namespace binding_control_example
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.set1 = new System.Data.DataSet();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(490, 246);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(216, 31);
+            this.textBox1.TabIndex = 0;
+            // 
+            // bindingSource1
+            // 
+            this.bindingSource1.BindingComplete += new System.Windows.Forms.BindingCompleteEventHandler(this.bindingSource1_BindingComplete);
+            // 
+            // set1
+            // 
+            this.set1.DataSetName = "NewDataSet";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(87, 246);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(216, 31);
+            this.textBox2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            
+            this.dataGridView1.DataSource = this.textBox1.Controls;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 137);
+            this.dataGridView1.TabIndex = 2;
+            //
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.set1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+              
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private BindingSource bindingSource1;
+        private DataGridView dataGridView1;
+        public DataSet set1;                
+    }
+}
