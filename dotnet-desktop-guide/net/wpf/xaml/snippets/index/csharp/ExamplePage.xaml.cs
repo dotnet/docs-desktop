1333d909d4d98e6abe110f2a4cff1307b3db38f4
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2023-01-31 15:54:35 -0800

    Add new XAML page that matches article (#1564)
    * Add new XAML page that matches article
    
    * minor
    
    * minor

---
+++ dotnet-desktop-guide/net/wpf/xaml/snippets/index/csharp/ExamplePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace ExampleNamespace;
+
+public partial class ExamplePage : Page
+{
+    public ExamplePage() =>
+        InitializeComponent();
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        var buttonControl = (Button)e.Source;
+        buttonControl.Foreground = Brushes.Red;
+    }
+}
---
+++ dotnet-desktop-guide/net/wpf/xaml/snippets/index/vb/ExamplePage.xaml.vb
@@ -0,0 +1,6 @@
+﻿Class ExamplePage
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        Dim buttonControl = DirectCast(e.Source, Button)
+        buttonControl.Foreground = Brushes.Red
+    End Sub
+End Class
