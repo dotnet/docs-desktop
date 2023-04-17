7bca297bff95d92651c0f720c4de417eef82f92f
Andy De George (adegeo) <adegeo@microsoft.com>
2023-04-17 12:38:20 -0700

    Update code example

---
+++ dotnet-desktop-guide/framework/wpf/controls/snippets/how-to-add-a-watermark-to-a-textbox/vb/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Class MainWindow
+    ' <code>
+    Private Sub OnTextBoxTextChanged(sender As Object, e As TextChangedEventArgs)
+        If TypeOf sender Is TextBox Then
+            Dim box As TextBox = DirectCast(sender, TextBox)
+
+            If String.IsNullOrEmpty(box.Text) Then
+                box.Background = DirectCast(FindResource("watermark"), ImageBrush)
+            Else
+                box.Background = Nothing
+            End If
+        End If
+    End Sub
+    ' </code>
+End Class
