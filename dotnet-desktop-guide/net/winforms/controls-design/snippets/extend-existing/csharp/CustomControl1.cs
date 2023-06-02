f814f302bc49f6a9a30bfb898b83239d3801b310
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2023-06-01 20:32:37 -0700

    Initial publish of custom controls and designers (#1631)
    * minor
    
    * overview+usercontrol
    
    * minor
    
    * actually add the file....... ha!
    
    * edits
    
    * more articles
    
    * fix toc
    
    * Add gif
    
    * minor
    
    * toc
    
    * Apply suggestions from code review
    
    Co-authored-by: Klaus Löffelmann <9663150+KlausLoeffelmann@users.noreply.github.com>
    
    * minor fix
    
    * Feedback
    
    * date
    
    * Apply suggestions from code review
    
    Co-authored-by: Merrie McGaw <mmcgaw@microsoft.com>
    
    * redirects
    
    ---------
    
    Co-authored-by: Klaus Löffelmann <9663150+KlausLoeffelmann@users.noreply.github.com>
    Co-authored-by: Merrie McGaw <mmcgaw@microsoft.com>

---
+++ dotnet-desktop-guide/net/winforms/controls-design/snippets/extend-existing/csharp/CustomControl1.cs
@@ -0,0 +1,38 @@
+﻿namespace CustomControlProject;
+
+//<control>
+public partial class CustomControl1 : Button
+{
+    //<counter>
+    private int _counter = 0;
+    //</counter>
+
+    public CustomControl1()
+    {
+        InitializeComponent();
+    }
+
+    //<onpaint>
+    protected override void OnPaint(PaintEventArgs pe)
+    {
+        // Draw the control
+        base.OnPaint(pe);
+
+        // Paint our string on top of it
+        pe.Graphics.DrawString($"Clicked {_counter} times", Font, Brushes.Purple, new PointF(3, 3));
+    }
+    //</onpaint>
+
+    //<onclick>
+    protected override void OnClick(EventArgs e)
+    {
+        // Increase the counter and redraw the control
+        _counter++;
+        Invalidate();
+
+        // Call the base method to invoke the Click event
+        base.OnClick(e);
+    }
+    //</onclick>
+}
+//</control>
---
+++ dotnet-desktop-guide/net/winforms/controls-design/snippets/extend-existing/csharp/CustomControl2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//<control>
+namespace CustomControlProject
+{
+    public partial class CustomControl2 : Control
+    {
+        public CustomControl2()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
+//</control>
