925bf4b9acb966307c27d4a191261a43069358b9
Andy De George (adegeo) <adegeo@microsoft.com>
2024-02-13 11:55:28 -0800

    Example test

---
+++ dotnet-desktop-guide/samples/testing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var results = Regex.Match("abc8xyzfdsa", "((?<One>abc)\\d+)?(?<Two>xyz)(.*)", RegexOptions.RightToLeft);
+            
+            int index = 0;
+            // foreach (var group in results.Groups) // .NET 7
+            foreach (var group in results.Groups.Cast<Group>()) // .NET 4.8
+            {
+                Console.WriteLine($"I:{index} N:{group.Name} V:{group.Value}");
+                index++;
+            }
+        }
+    }
+}
