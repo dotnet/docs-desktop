003ad7f213ba8af3602718a93c3c06999cfdc523
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2024-06-12 13:57:49 -0700

    Update Form1.cs

--- dotnet-desktop-guide/net/winforms/printing/snippets/how-to-print-windows-form/csharp/Form1.cs
+++ dotnet-desktop-guide/net/winforms/printing/snippets/how-to-print-windows-form/csharp/Form1.cs
@@ -15,13 +15,15 @@ namespace Sample_print_win_form1
             memoryImage = new Bitmap(s.Width, s.Height, myGraphics);
             Graphics memoryGraphics = Graphics.FromImage(memoryImage);
             memoryGraphics.CopyFromScreen(this.Location.X, this.Location.Y, 0, 0, s);
-            
+
             printDocument1.Print();
-            
         }
-        private void PrintDocument1_PrintPage(System.Object sender,
-           System.Drawing.Printing.PrintPageEventArgs e) =>
-               e.Graphics.DrawImage(memoryImage, 0, 0);
-        
+
+        private void PrintDocument1_PrintPage(
+           System.Object sender,
+           System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            e.Graphics.DrawImage(memoryImage, 0, 0);
+        }
     }
 }
