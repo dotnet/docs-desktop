d13814925571ef1c0646e8b65205050f0d7244fc
Andy De George (adegeo) <adegeo@microsoft.com>
2024-06-13 18:48:20 -0700

    Update example

---
+++ dotnet-desktop-guide/framework/wpf/app-development/snippets/persist-and-restore-application-scope-properties/vb/MainWindow.xaml.vb
@@ -0,0 +1,5 @@
+﻿Public Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        lblTimes.Content = Application.Current.Properties("NumberOfAppSessions")
+    End Sub
+End Class
