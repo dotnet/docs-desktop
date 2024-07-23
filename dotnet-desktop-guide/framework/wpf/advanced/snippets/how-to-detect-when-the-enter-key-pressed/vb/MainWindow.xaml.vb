2e1796116f32ca93f34d5466f1835d5853d59c7e
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2024-07-23 09:47:05 -0700

    Change RETURN to ENTER in WPF example (#1867)
    * update article; move snippets
    
    * Update dotnet-desktop-guide/framework/wpf/advanced/how-to-detect-when-the-enter-key-pressed.md
    
    * Update dotnet-desktop-guide/framework/wpf/advanced/how-to-detect-when-the-enter-key-pressed.md

---
+++ dotnet-desktop-guide/framework/wpf/advanced/snippets/how-to-detect-when-the-enter-key-pressed/vb/MainWindow.xaml.vb
@@ -0,0 +1,18 @@
+﻿'<full>
+Imports System.Threading
+
+Public Class MainWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+    End Sub
+
+    '<handler>
+    Private Sub textBox1_KeyDown(sender As Object, e As System.Windows.Input.KeyEventArgs)
+
+        If e.Key = Key.Return Then
+            textBlock1.Text = "You Entered: " + textBox1.Text
+        End If
+
+    End Sub
+    '</handler>
+End Class
+'</full>
