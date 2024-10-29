670d3b5dfbbd80ae9118c1c5ae0549627324673d
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2024-10-28 17:44:23 -0700

    Fix binding validation path (#1916)
    * Fix binding validation path
    
    * missing file
    
    * DUHH it's a key, not a name
    
    * add missing config

--- dotnet-desktop-guide/samples/snippets/csharp/VS_Snippets_Wpf/BindValidation/CSharp/MyDataSource.cs
+++ dotnet-desktop-guide/samples/snippets/csharp/VS_Snippets_Wpf/BindValidation/CSharp/MyDataSource.cs
@@ -2,33 +2,18 @@
 
 namespace SDKSample
 {
+    //<DataSource>
     public class MyDataSource
     {
-        private int _age;
-        private int _age2;
-        private int _age3;
-
         public MyDataSource()
         {
             Age = 0;
             Age2 = 0;
         }
 
-        public int Age
-        {
-            get { return _age; }
-            set { _age = value; }
-        }
-        public int Age2
-        {
-            get { return _age2; }
-            set { _age2 = value; }
-        }
-
-        public int Age3
-        {
-            get { return _age3; }
-            set { _age3 = value; }
-        }
+        public int Age { get; set; }
+        public int Age2 { get; set; }
+        public int Age3 { get; set; }
     }
+    //</DataSource>
 }
