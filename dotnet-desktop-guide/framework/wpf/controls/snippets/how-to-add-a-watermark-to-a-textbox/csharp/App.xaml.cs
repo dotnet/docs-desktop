4e872be2ed15ded378d9c72630a022bfccf8eb79
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2025-03-21 14:22:21 -0700

    Rewrite .NET Framework article + code + screens (#2042)
    * Rewrite .NET Framework article + code + screens
    
    * Apply suggestions from code review
    
    Co-authored-by: Bill Wagner <wiwagn@microsoft.com>
    
    * Update dotnet-desktop-guide/framework/wpf/controls/how-to-add-a-watermark-to-a-textbox.md
    
    Co-authored-by: Bill Wagner <wiwagn@microsoft.com>
    
    ---------
    
    Co-authored-by: Bill Wagner <wiwagn@microsoft.com>

--- dotnet-desktop-guide/framework/wpf/controls/snippets/how-to-add-a-watermark-to-a-textbox/csharp/App.xaml.cs
+++ dotnet-desktop-guide/framework/wpf/controls/snippets/how-to-add-a-watermark-to-a-textbox/csharp/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace watermark
+namespace DotnetDocsSample
 {
     /// <summary>
     /// Interaction logic for App.xaml
