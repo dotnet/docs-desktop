23cb1f8a364f8db0addf09551d7d6b618e872905
Andy De George (from Dev Box) <adegeo@microsoft.com>
2025-10-29 08:48:26 -0700

    Add project (temp?) that has every control in it

---
+++ dotnet-desktop-guide/wpf/controls/snippets/shared/reference/csharp/Pages/MenuPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace AllTemplatesCS.Pages
+{
+    public partial class MenuPage : Page
+    {
+        public MenuPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ dotnet-desktop-guide/wpf/controls/snippets/shared/reference/csharp/Pages/PasswordBoxPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace AllTemplatesCS.Pages
+{
+    public partial class PasswordBoxPage : Page
+    {
+        public PasswordBoxPage()
+        {
+            InitializeComponent();
+            // Set a sample password for demonstration
+            DefaultPassword.Password = "sample123";
+        }
+    }
+}
