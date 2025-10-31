8238ba78b346ca564a9316b402c2edff278a0838
Andy De George (from Dev Box) <adegeo@microsoft.com>
2025-10-31 11:04:58 -0700

    temp

--- dotnet-desktop-guide/winforms/migration/snippets/clipboard-dataobject-net10/net/csharp/Form1.Designer.cs
+++ dotnet-desktop-guide/winforms/migration/snippets/clipboard-dataobject-net10/net/csharp/Form1.Designer.cs
@@ -28,11 +28,31 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
-        components = new System.ComponentModel.Container();
+        button1 = new Button();
+        SuspendLayout();
+        // 
+        // button1
+        // 
+        button1.Location = new Point(39, 31);
+        button1.Name = "button1";
+        button1.Size = new Size(112, 34);
+        button1.TabIndex = 0;
+        button1.Text = "button1";
+        button1.UseVisualStyleBackColor = true;
+        button1.Click += button1_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new SizeF(10F, 25F);
         AutoScaleMode = AutoScaleMode.Font;
         ClientSize = new Size(800, 450);
+        Controls.Add(button1);
+        Name = "Form1";
         Text = "Form1";
+        ResumeLayout(false);
     }
 
     #endregion
+
+    private Button button1;
 }
--- dotnet-desktop-guide/winforms/migration/snippets/clipboard-dataobject-net10/net/csharp/Form1.cs
+++ dotnet-desktop-guide/winforms/migration/snippets/clipboard-dataobject-net10/net/csharp/Form1.cs
@@ -1,9 +1,16 @@
-namespace csharp;
+﻿namespace csharp;
 
 public partial class Form1 : Form
 {
     public Form1()
     {
         InitializeComponent();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        ClipboardExamples.ObsoletePatterns.BrokenCustomTypeExample();
+        ClipboardExamples.ObsoletePatterns.ObsoleteGetDataExample();
+        
     }
 }
