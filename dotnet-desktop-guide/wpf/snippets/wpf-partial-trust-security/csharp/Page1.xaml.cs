5b136d7e904ce4faa0b36602f99f86b8a223d568
Andy (Steve) De George <67293991+adegeo@users.noreply.github.com>
2026-07-20 12:34:03 -0700

    Migrate remaining WPF legacy snippets (#2265)
    * Migrate code
    
    * Convert VB projs to VS
    
    * Fix various presentation errors
    
    * Delete old snippets

---
+++ dotnet-desktop-guide/wpf/snippets/wpf-partial-trust-security/csharp/Page1.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace WpfPartialTrustSecurity
+{
+    public partial class Page1 : Page
+    {
+        public Page1()
+        {
+            InitializeComponent();
+        }
+    }
+}
